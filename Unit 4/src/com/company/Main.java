28327825dd38d06f244f4ae7237978a07e63ba1d
MatroskeN <piranya1399@gmail.com>
2018-12-19 15:41:47 +0400

    Ныаааа

--- Unit 4/src/com/company/Main.java
+++ Unit 4/src/com/company/Main.java
@@ -6,23 +6,27 @@ public class Main {
     public static void main(String[] args) {
 	    Person vlad = new Person();
         System.out.println("Высота Влада - "+vlad.height+" сантиметра");
-        //func (3,4);
-	    //readArray();
+        System.out.println("Вес Влада - "+vlad.weight+" килограмм");
+        func (2,2);
+	    readArray();
     }
-    //public static int [] readArray (){
-        //int[] arr;
-        //int n;
-        //Scanner name = new Scanner(System.in);
-        //n = name.nextInt();
-        //arr = new int [n];
-        //for (int i = 0; i < n; i++)
-            //arr[i] = name.nextInt();
-        //return arr;
-    //}
-    //public static void func (int a, int b) {
-        //int result = 1;
-        //for (int i = 0; i < b; i++)
-            //result *= a;
-        //System.out.println(result);
-    //}
+    public static int [] readArray (){
+        int[] arr;
+        int n;
+        System.out.println("Введите размерность массива");
+        Scanner name = new Scanner(System.in);
+        n = name.nextInt();
+        System.out.println("Заполните массив");
+        arr = new int [n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = name.nextInt();
+        }
+        return arr;
+    }
+    public static void func (int a, int b) {
+        int result = 1;
+        for (int i = 0; i < b; i++)
+            result *= a;
+        System.out.println(result);
+    }
 }
--- Unit 4/src/com/company/Person.java
+++ Unit 4/src/com/company/Person.java
@@ -2,4 +2,5 @@ package com.company;
 
 public class Person {
     public int height = 182;
+    public int weight = 87;
 }
